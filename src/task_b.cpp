d881a124b44afe704b4b7d26f981cb3abe98717b
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-13 08:19:33 +1100

    Update task_b.cpp

---
+++ src/task_b.cpp
@@ -0,0 +1,10 @@
+#include "task_b.h"
+
+void build_buildings() {
+    std::cout << "\t Building the exterior of the buildings..." << std::endl ;
+
+    std::cout << "\t Subdividing the interior..." << std::endl ;
+
+    std::cout << "\t Doing some interior decorating..." << std::endl ;
+
+}
