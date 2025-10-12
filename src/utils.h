f7455c357f135d4229664c5468854c947a7593b6
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-13 08:20:36 +1100

    Update utils.h

---
+++ src/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+
+void print_student_details();
+void print_command_line_arguments(int loc_x, int loc_z, int village_size, int plot_border, int seed, bool testmode);
+
+#endif
