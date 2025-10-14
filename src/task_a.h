476a0f7287207df510a5e5b07da0b5d86b64071c
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-14 15:01:03 +1100

    Update task_a.h

--- src/task_a.h
+++ src/task_a.h
@@ -2,13 +2,20 @@
 #define TASK_A
 
 #include <vector>
+#include <mcpp/mcpp.h>
+#include "models/Plot/plot.h" 
 
-#include "plots.h"
+int get_surface_y(mcpp::MinecraftConnection& mc, int x, int z) ;
+
+bool validate_plot(mcpp::MinecraftConnection& mc, const mcpp::Coordinate& origin, const mcpp::Coordinate& bound, const std::vector<Plot>& existing_plots) ;
+
+mcpp::Coordinate determine_entrance(const mcpp::Coordinate& origin, const mcpp::Coordinate& bound) ;
+
+std::vector<mcpp::Coordinate> find_waypoints(const std::vector<Plot>& plots) ;
 
 std::vector<Plot> find_plots() ;
-
-void terraform() ;
-
+void terraform(const std::vector<Plot>& plots) ;
 void place_wall() ;
 
 #endif
+
