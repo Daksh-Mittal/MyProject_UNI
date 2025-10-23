bd0af23b7f15319486015abe8ccf3a1f0343f195
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 01:56:46 +1100

    Update task_a.h

--- src/task_a.h
+++ src/task_a.h
@@ -1,21 +1,24 @@
-#ifndef TASK_A
-#define TASK_A
+#ifndef TASK_A_H
+#define TASK_A_H
 
 #include <vector>
 #include <mcpp/mcpp.h>
 #include "models/Plot/plot.h" 
 
-int get_surface_y(mcpp::MinecraftConnection& mc, int x, int z) ;
+// Helper functions
+int get_surface_y(mcpp::MinecraftConnection& mc, int x, int z);
+bool validate_plot(mcpp::MinecraftConnection& mc, const mcpp::Coordinate& origin, 
+                   const mcpp::Coordinate& bound, const std::vector<Plot>& existing_plots);
+mcpp::Coordinate determine_entrance(const mcpp::Coordinate& origin, const mcpp::Coordinate& bound);
+void build_wall_at(mcpp::MinecraftConnection& mc, int x, int z);
+void fast_build_wall_at(mcpp::MinecraftConnection& mc, int x, int z);
 
-bool validate_plot(mcpp::MinecraftConnection& mc, const mcpp::Coordinate& origin, const mcpp::Coordinate& bound, const std::vector<Plot>& existing_plots) ;
-
-mcpp::Coordinate determine_entrance(const mcpp::Coordinate& origin, const mcpp::Coordinate& bound) ;
-
-std::vector<mcpp::Coordinate> find_waypoints(const std::vector<Plot>& plots) ;
-
-std::vector<Plot> find_plots() ;
-void terraform(const std::vector<Plot>& plots) ;
-void place_wall() ;
+// Main Task A functions
+std::vector<Plot> find_plots();
+void terraform(const std::vector<Plot>& plots);
+void place_wall(const std::vector<Plot>& plots);
+std::vector<mcpp::Coordinate> find_waypoints(const std::vector<Plot>& plots);
 
 #endif
+
 
