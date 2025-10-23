6ab2049ba6c5c9bf53e230373596f65393daf6f5
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 02:02:23 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,69 +3,86 @@
 #include <iostream>
 #include <ctime>
 
-#include "plots.h"
+#include "models/Plot/plot.h"
 #include "utils.h"
 #include "task_a.h"
 #include "task_b.h"
 #include "task_c.h"
+#include "config.h"
 
-int main(const int argc, const char *const *const argv) {
-    print_student_details() ;
+int main(const int argc, const char *argv[]) {
+    int returnCode = 0;
+    mcpp::MinecraftConnection mc;
+    printStudentDetails();
 
-    // MinecraftConnection mc;
+    try {
+        Config::GetInstance().ApplyConfiguration(argc, argv);
+        Config::GetInstance().SetMinecraftConnection(&mc);
+        std::srand(Config::GetInstance().GetSeed());
 
-    // Read command line arguments:
+        if (Config::GetInstance().IsTestMode()) {
+            std::string component = Config::GetInstance().GetTestedComponentName();
+            int testCase = Config::GetInstance().GetTestCase();
+            std::cout << "Testing component '" << component << '\'';
+            if (testCase != -1) std::cout << " with test case " << testCase;
+            std::cout << std::endl << std::endl;
 
-    int loc_x = 0 ;
-    int loc_z = 0 ;
-    int village_size = 0 ;
-    int plot_border = 0 ;
-    int seed = 0 ;
-    bool testmode = false ;
+            if (component == "exterior" || component == "subdivision" || component == "furnishing") {
+                testTaskB();
+            }
+            else if (component == "pathfinding" || component == "connect_buildings") {
+                testTaskC(component, testCase, mc);
+            }
 
-    // Demonstrating how to read command line arguments: REMOVE IN FINAL PROGRAM
-    std::cout << "There are " << argc << " command line arguments." << std::endl ;
-    for (unsigned int i = 0 ; i < argc ; i++) {
-        std::cout << argv[i] << std::endl ;
+        }
+        else {
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // Task A
+            std::cout << "Task A: Finding Plots and Terraforming" << std::endl ;
+
+            std::cout << "\t Finding plots..." << std::endl ;
+
+            // Insert finding plots here.
+            std::vector<Plot> plots = find_plots() ;
+
+            std::cout << "\t Doing some landscaping..." << std::endl ;
+
+            // Insert terraforming here.
+            terraform() ;
+
+            // Insert Placing a wall here.
+            std::cout << "\t Placing a cool wall around the village..." << std::endl ;
+            place_wall() ;
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // Task B
+            std::cout << "Task B: Building Houses" << std::endl ;
+            buildBuildings(pointerisePlotVector(plots));
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // Task C
+            std::cout << "Task C: Building Houses" << std::endl ;
+            std::vector<Plot> p = find_plots();
+            std::vector<Waypoint> waypoints = get_waypoints();
+            mcpp::Coordinate start(waypoints[0].x, waypoints[0].y, waypoints[0].z);
+
+            std::cout << "\t Connecting waypoints with a fancy path..." << std::endl ;
+            connect_waypoints(start, waypoints, mc);
+
+            std::cout << "\t Connecting buildings to waypoints with more fancy paths..." << std::endl ;
+            connect_buildings(waypoints, p, mc);
+        }
+    }
+    catch(std::exception exception) {
+        std::cerr << exception.what() << std::endl;
+        returnCode = 1;
     }
 
-    print_command_line_arguments(loc_x, loc_z, village_size, plot_border, seed, testmode) ;
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Task A
-    std::cout << "Task A: Finding Plots and Terraforming" << std::endl ;
-
-    std::cout << "\t Finding plots..." << std::endl ;
-
-    // Insert finding plots here.
-    std::vector<Plot> plots = find_plots() ;
-
-    std::cout << "\t Doing some landscaping..." << std::endl ;
-
-    // Insert terraforming here.
-    terraform() ;
-
-    // Insert Placing a wall here.
-    std::cout << "\t Placing a cool wall around the village..." << std::endl ;
-    place_wall() ;
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Task B
-    std::cout << "Task B: Building Houses" << std::endl ;
-    build_buildings() ;
-
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Task C
-    std::cout << "Task C: Building Houses" << std::endl ;
-
-    std::cout << "\t Connecting waypoints with a fancy path..." << std::endl ;
-    connect_waypoints() ;
-
-    std::cout << "\t Connecting buildings to waypoints with more fancy paths..." << std::endl ;
-    connect_buildings() ;
+    return returnCode;
+}
 
 
 
     return 0;
 }
+
