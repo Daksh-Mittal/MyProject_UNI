7998ed5069d05dfe2b908d6d35502a7787540c09
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 01:58:32 +1100

    Update task_b.h

--- src/task_b.h
+++ src/task_b.h
@@ -3,9 +3,21 @@
 
 #include <vector>
 #include <iostream>
-#include "plots.h"
+#include <map>
+#include "models/PlotRegion/plot_region.h"
+#include "models/Plot/plot.h"
+#include "models/Room/room.h"
+#include "canvas.h"
+#include "utils.h"
 
-void build_buildings() ;
+void testTaskB();
+void buildBuildings(std::vector<Plot*> plots);
 
+void buildExterior(Plot* plot, Canvas& canvas);
+std::unique_ptr<Floor> subdivideInterior(Plot* plot, Canvas& canvas);
+void decorateInterior(std::unique_ptr<Floor>& floor, Canvas& canvas);
+
+std::vector<PlotRegion> subdividePlot(const Plot& plot);
+std::vector<PlotRegion> subdivide(std::vector<PlotRegion> regions);
 
 #endif
