fbce5391aac339a81fd08e318883f83e729e8dfc
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 14:44:50 +1100

    Update prim.h

---
+++ src/models/PrimAlgorithm/prim.h
@@ -0,0 +1,13 @@
+#ifndef PRIM_H
+#define PRIM_H
+#include <vector>
+#include <map>
+#include <mcpp/mcpp.h>
+#include "models/Waypoints/waypoint.h"
+
+std::vector<Waypoint> Prim(mcpp::Coordinate start, std::vector<Waypoint> waypoints);
+std::map<Waypoint, std::vector<Waypoint>> createAJL(std::vector<Waypoint>& parent, std::vector<Waypoint>& waypoints);
+double calc_dist(int u, int v, const std::vector<Waypoint>& waypoints);
+int minimum(std::vector<double>& key, std::vector<bool>& mst);
+
+#endif
