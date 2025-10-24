c93166b56fa73352bb0b8005fa7aad943d88690e
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 14:35:11 +1100

    Update room_relationship.h

--- src/models/RoomRelationship/room_relationship.h
+++ src/models/RoomRelationship/room_relationship.h
@@ -1,16 +1,21 @@
 #ifndef ROOM_RELATIONSHIP_H
 #define ROOM_RELATIONSHIP_H
 
+#include <mcpp/mcpp.h>
+#include "enums/side.h"
+
 // forward declaration to avoid circular dependency with Room
 class Room;
 
 struct RoomRelationship {
-  Room *Room1;
-  Room *Room2;
+  Room* Room1;
+  Room* Room2;
   mcpp::Coordinate ConnectionPoint;
+  Side Orientation;
   bool DoorBuilt = false;
 
-  RoomRelationship(Room, Room);
+  RoomRelationship(Room*, Room*);
 };
 
 #endif
+
