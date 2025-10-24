b25e9393dfdc34bd1fc3e8ca1bffc620dd2b24da
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-24 14:45:18 +1100

    Update waypoint.cpp

---
+++ src/models/Waypoints/waypoint.cpp
@@ -0,0 +1,8 @@
+#include "models/Waypoints/waypoint.h"
+std::vector<Waypoint> get_waypoints() {
+    return {
+      {125, 71, 175, false},
+      {142, 71, 159, false},
+      {112, 72, 160, false}
+    };
+}
