336ebabf9e980b5f0e4c6a7c8e8497900524894c
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-25 14:50:21 +1100

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -2,12 +2,14 @@
 #define UTILS_H
 
 #include <iostream>
+#include <mcpp/mcpp.h>
 #include "models/Plot/plot.h"
 #include "config.h"
 
 void printStudentDetails();
 bool isTesting(std::string testingComponent);
 std::vector<Plot*> pointerisePlotVector(std::vector<Plot> plots);
+void clearEntrance(Plot* plot);
+void moveTo(mcpp::Coordinate from, mcpp::Coordinate to);
 
 #endif
-
