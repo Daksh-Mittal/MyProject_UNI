3accee48f454518b123634a8383459fd52850788
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-25 14:49:56 +1100

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -19,3 +19,43 @@ std::vector<Plot*> pointerisePlotVector(std::vector<Plot> plots) {
 
   return plotPointers;
 }
+
+void moveTo(mcpp::Coordinate from, mcpp::Coordinate to) {
+  mcpp::MinecraftConnection* mc = Config::GetInstance().GetMinecraftConnection();
+  mcpp::BlockType block = mc->getBlock(from);
+  
+  mc->setBlock(from, mcpp::Blocks::AIR);
+  mc->setBlock(to, block);
+}
+
+void clearEntrance(Plot* plot) {
+  mcpp::MinecraftConnection* mc = Config::GetInstance().GetMinecraftConnection();
+  mcpp::Coordinate entrance = plot->entrance;
+
+  // Left
+  if (entrance.x == plot->origin.x) {
+    mcpp::Coordinate check = entrance + mcpp::Coordinate(1, 0, 0);
+
+    if (mc->getBlock(check) != mcpp::Blocks::AIR) {
+      moveTo(check, check + mcpp::Coordinate(0, 0, 1));
+      moveTo(check + mcpp::Coordinate(0, 1, 0), check + mcpp::Coordinate(0, 1, 1));
+    }
+  }
+  // Right
+  else if (entrance.x == plot->bound.x) {
+    // do nothing - there should never be obstacles
+  }
+  // Top
+  else if (entrance.y == plot->origin.y) {
+    mcpp::Coordinate check = entrance + mcpp::Coordinate(0, 0, 1);
+
+    if (mc->getBlock(check) != mcpp::Blocks::AIR) {
+      moveTo(check, check + mcpp::Coordinate(1, 0, 0));
+      moveTo(check + mcpp::Coordinate(0, 1, 0), check + mcpp::Coordinate(1, 1, 0));
+    }
+  }
+  // Bottom
+  else if (entrance.y == plot->bound.y) {
+    // do nothing - there should never be obstacles
+  }
+}
