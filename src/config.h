789edd41b492a6a61fa73ffef5b5c0bfa36c1d32
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2025-10-28 15:45:38 +1100

    Update config.h

--- src/config.h
+++ src/config.h
@@ -13,11 +13,12 @@ class Config {
     void ApplyConfiguration(const int argc, const char *argv[]);
     void ApplyConfiguration(std::map<std::string, std::string> config);
     void SetMinecraftConnection(mcpp::MinecraftConnection* mc);
+    void SetLocation(mcpp::Coordinate* location); // ADDED: Setter for default location
 
     bool IsTestMode() const;
     std::string GetTestedComponentName() const;
     int GetTestCase() const;
-    mcpp::Coordinate2D* GetLocation() const;
+    mcpp::Coordinate* GetLocation() const; // Changed from mcpp::Coordinate2D* to mcpp::Coordinate* for consistency
     int GetVillageSize() const;
     int GetPlotBorder() const;
     int GetSeed() const;
@@ -27,7 +28,7 @@ class Config {
     Config();
 
     std::string testingComponent = "";
-    mcpp::Coordinate2D* location = nullptr;
+    mcpp::Coordinate* location = nullptr; // Adjusted type for clarity, though it seems it was intended to store 3D coords
     mcpp::MinecraftConnection* mc = nullptr;
     bool isTestMode = false;
     int testCase = -1;
